d3969e9b87cfe8b164a2ed6fb98063ed37cc599a
Quốc Hào <34256891+quochao0906@users.noreply.github.com>
2018-12-07 22:14:58 +0700

    a

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -117,14 +117,14 @@
 <!-- /banner_bottom_agile_info -->
 <div class="page-head_agile_info_w3l">
 		<div class="container">
-			<h3>About <span>Us </span></h3>
+			<h3>Giới Thiệu </h3>
 			<!--/w3_short-->
 				 <div class="services-breadcrumb">
 						<div class="agile_inner_breadcrumb">
 
 						   <ul class="w3_short">
-								<li><a href="index.html">Home</a><i>|</i></li>
-								<li>About</li>
+								<li><a href="home">Trang Chủ</a><i>|</i></li>
+								<li>Giới Thiệu</li>
 							</ul>
 						 </div>
 				</div>
